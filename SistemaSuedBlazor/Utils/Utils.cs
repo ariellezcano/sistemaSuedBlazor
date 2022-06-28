1bfe41f4a5794c28251f0a174b90dc23619a56ff
ariellezcano <ariellezcano1995@gmail.com>
2022-06-28 09:05:23 -0300

    Agregar archivos de proyecto.

---
+++ SistemaSuedBlazor/Utils/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaSuedBlazor.Util
+{
+    public class Utils
+    {
+        public static string URL_registroUsuario = "http://10.125.31.74/api_registroUsuario/";
+        public static string URLCivil = "http://10.125.31.74/api_civil/";
+
+
+
+    }
+}
