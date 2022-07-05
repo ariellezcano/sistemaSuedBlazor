00efb16c437cbddefd08cf4b0a94d099b6fe626b
ariellezcano <ariellezcano1995@gmail.com>
2022-07-05 13:44:40 -0300

    modificado

---
+++ SistemaSuedBlazor/Models/Objeto.models.cs
@@ -0,0 +1,12 @@
+﻿namespace SistemaSuedBlazor.Models
+{
+    public class ObjetoModel
+    {
+        public string objeto { get; set; }
+        public int cantidad { get; set; }
+        public int valor { get; set; }
+        public bool secuestro { get; set; }
+        public DateTime fecha { get; set; }
+        public int cantidadSec { get; set; }
+    }
+}
